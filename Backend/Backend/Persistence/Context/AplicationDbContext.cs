4cc7fc573807efaa7bcb4a8ba0c3143ddcc72041
David Zambrano <developerzambrano@outlook.com>
2020-10-25 23:09:05 -0600

    Questions & Answers NetCore Solution
    Creación e iniciación del backend

---
+++ Backend/Backend/Persistence/Context/AplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Backend.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Persistence.Context
+{
+    /// <summary>
+    /// Contexto utilizado para conexion hacia base de datos
+    /// </summary>
+    public class AplicationDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        //Constructor
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
